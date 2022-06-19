e332268532c701aad399bb4d23a7f5b8a489fdb2
Maxim Grishaev <maxim.grishaev@gmail.com>
2022-06-20 01:53:03 +0300

    replace logo, rm favicon.png

--- apps/web/src/components/home/WhoWeAreSection.tsx
+++ apps/web/src/components/home/WhoWeAreSection.tsx
@@ -1,8 +1,6 @@
 import { Box } from '@mui/material';
-import Image from 'next/image';
+import { LogoMenu } from 'ui/assets';
 import { Button, Col, H2, Para, Section } from 'ui/ux';
-
-import Logo from '../../../public/img/logo.png';
 
 export const WhoWeAreSection = () => {
   return (
@@ -22,7 +20,7 @@ export const WhoWeAreSection = () => {
 
         <Col.Item sm={6} vAlign="center">
           <Box mt={{ xs: 8, sm: 0 }}>
-            <Image src={Logo} />
+            <LogoMenu width={511} height={151} />
           </Box>
         </Col.Item>
       </Col.Container>
